dd50c06556a5e522616c99df1fa3180b374114ff
Khaino <pskhai@gmail.com>
2020-04-26 14:31:01 +0800

    Updates

---
+++ precache-manifest.a159e93a3e3e403aed3f183f2e84ac36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c136f963967916bd0044539413f4ce80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "228092806fc25da702bc",
+    "url": "/monsters-rolodex/static/css/main.6478d52d.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228092806fc25da702bc",
+    "url": "/monsters-rolodex/static/js/main.99a86a2c.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
